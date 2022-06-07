b7ba637bb7208cc97916c73efcc16e541b159e7f
aaron <1020069668@qq.com>
2022-06-07 16:21:04 +0800

    修改app目录的结构

---
+++ app/weapp/controller/Index.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\weapp\controller;
+
+use app\common\BaseController;
+
+class Index extends BaseController
+{
+    public function index()
+    {
+        return 'hello';
+    }
+}
